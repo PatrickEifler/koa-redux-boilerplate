05d184cde216d93ad5baf83b5c41337baf36b44b
PatrickEifler <patrickeifler@gmail.com>
2016-08-18 14:52:38 +0200

    Add linting and cleanup dist and build

---
+++ Readme.md
@@ -0,0 +1,3 @@
+npm run webserver - will start the api at port 3000
+npm run dev - will start the webpack-dev-server and thus start the app at 8080
+Make API calls from the app to the port 3000, where the backend server is listening.
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,21 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: process.env.NODE_ENV === 'production' ? '' : 'source-map',
   entry: path.resolve(ROOT_PATH, 'app/src/index'),
   output: {
-    path: path.resolve(ROOT_PATH, 'app/dist'),
+    path: process.env.NODE_ENV === 'production' ? path.resolve(ROOT_PATH, 'dist') : path.resolve(ROOT_PATH, 'build'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.js?$/,
+        loaders: process.env.NODE_ENV === 'production' ? [] : ['eslint'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ],
     loaders: [
       {
         test: /\.js?$/,
@@ -28,7 +35,7 @@ module.exports = {
     extensions: ['', '.js']
   },
   devServer: {
-    contentBase: path.resolve(ROOT_PATH, 'app/dist'),
+    contentBase: path.resolve(ROOT_PATH, 'build'),
     historyApiFallback: true,
     hot: true,
     inline: true,
